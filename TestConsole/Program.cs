921ee634204f1e7e07cc1dbe55d37c19c9b32705
warmsun <979539341@qq.com>
2019-07-19 10:48:09 +0800

    增加测试控制台项目

---
+++ TestConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (int item in yieldTest.GetList(5))
+                Console.Write(item);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ TestConsole/yieldTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestConsole
+{
+    public static class yieldTest
+    {
+        static Random r = new Random();
+        public static IEnumerable<int> GetList(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                yield return r.Next(10);
+            }
+        }
+    }
+}
